093d4a026d492374bb19f79ce6a1caa065b82cbd
Isabelle Faculdade <isabelleribeirodossantos12@gmail.com>
2026-09-13 19:09:00 -0300

    Corrige duplicação de variáveis no Main e adiciona teste com linha tocando as bordas

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
         FloodFill floodFill = new FloodFill();
 
         String imagemEntradaPilha = "src/imagens/PILHA.png";
+        String imagemEntradaPilha2 = "src/imagens/Pilha2.png";
         String imagemEntradaFila = "src/imagens/Fila.png";
         int linhaInicial = 5;
         int colunaInicial = 5;
         int novaCor = 0xFFFF69B4;
         int intervaloFrames = 2000;
+        int intervaloFrames2 = 5000;
 
         System.out.println("Processando com Pilha...");
         BufferedImage imagemPilha = gerenciador.abrirImagem(imagemEntradaPilha);
@@ -19,6 +21,14 @@ public class Main {
         floodFill.preencherComAnimacao(imagemPilha, linhaInicial, colunaInicial, novaCor, pilha, "src/imagens/frames_pilha", intervaloFrames);
         gerenciador.salvarImagem(imagemPilha, "src/imagens/resultado_pilha.png");
         System.out.println("Concluído! Resultado salvo em resultado_pilha.png");
+
+
+        System.out.println("Processando com Pilha 2 (linha tocando as bordas)...");
+        BufferedImage imagemPilha2 = gerenciador.abrirImagem(imagemEntradaPilha2);
+        EstruturaDeDados pilha2 = new PilhaPropria(imagemPilha2.getWidth() * imagemPilha2.getHeight() * 4);
+        floodFill.preencherComAnimacao(imagemPilha2, linhaInicial, colunaInicial, novaCor, pilha2, "src/imagens/frames_pilha2", intervaloFrames);
+        gerenciador.salvarImagem(imagemPilha2, "src/imagens/resultado_pilha2.png");
+        System.out.println("Concluído! Resultado salvo em resultado_pilha2.png");
 
 
         System.out.println("Processando com Fila...");
